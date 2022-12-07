743ceae1b9d12765abc596ac6bc89b36a9be1b83
KamiloJ12 <camilo12052000@hotmail.com>
2022-12-07 14:19:29 -0500

    actualizar rutas

--- Gym/src/app/app-routing.module.ts
+++ Gym/src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { BodyComponent } from './components/body/body.component';
 import { LoginComponent } from './components/login/login.component';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'inicio', component: BodyComponent }
+  { path: 'inicio', component: BodyComponent },
   { path: 'administrador', children: [
     { path: 'autenticacion', component: LoginComponent  }
   ]}
@@ -13,8 +12,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
