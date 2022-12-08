e1a53b0c887dc9c4432caeab6a9c6bf6cc97b637
KamiloJ12 <camilo12052000@hotmail.com>
2022-12-07 19:33:19 -0500

    update

--- Gym/src/app/app.module.ts
+++ Gym/src/app/app.module.ts
@@ -6,12 +6,19 @@ import { BodyComponent } from './components/body/body.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { CommonModule } from '@angular/common';
 import { ClientesComponent } from './components/administrador/clientes/clientes.component';
 import { EntrenadorComponent } from './components/administrador/entrenador/entrenador.component';
 
 import { TableModule } from 'primeng/table';
 import { InputTextModule } from 'primeng/inputtext';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @NgModule({
   declarations: [
@@ -24,10 +31,17 @@ import { InputTextModule } from 'primeng/inputtext';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     CommonModule,
     TableModule,
-    InputTextModule
+    InputTextModule,
+    ToolbarModule,
+    ToastModule,
+    ButtonModule,
+    DialogModule,
+    ConfirmDialogModule,
+    InputTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Gym/src/app/components/administrador/clientes/clientes.component.ts
+++ Gym/src/app/components/administrador/clientes/clientes.component.ts
@@ -1,13 +1,118 @@
 import { Component } from '@angular/core';
+import { ConfirmationService } from 'primeng/api';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.css']
+  styleUrls: ['./clientes.component.css'],
+  providers: [MessageService,ConfirmationService]
 })
 export class ClientesComponent {
 
+  clienteDialog: boolean = false;
   clientes: any[] = [];
-  clientesSeleccionados: any[] = [];
-  loading: boolean = false;
+  cliente: any = {};
+  selectedClientes: any[] = [];
+  submitted: boolean = false;
+  statuses: any[] = [];
+
+  constructor(private messageService: MessageService, private confirmationService: ConfirmationService) { 
+    this.clientes = [
+      { id: 1, nombre: 'Brayan', apellido: 'Macias', cedula: 185249 },
+      { id: 2, nombre: 'Brayan', apellido: 'Macias', cedula: 185245 }
+    ]
+  }
+
+  ngOnInit() {
+    this.statuses = [
+      { label: 'INSTOCK', value: 'instock' },
+      { label: 'LOWSTOCK', value: 'lowstock' },
+      { label: 'OUTOFSTOCK', value: 'outofstock' }
+    ];
+  }
+
+  openNew() {
+    this.cliente = {};
+    this.submitted = false;
+    this.clienteDialog = true;
+  }
+
+  deleteSelectedClientes() {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete the selected clientes?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.clientes = this.clientes.filter(val => !this.selectedClientes.includes(val));
+        this.selectedClientes = [];
+        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Clientes Deleted', life: 3000 });
+      }
+    });
+  }
+
+  editCliente(cliente: any) {
+    this.cliente = { ...cliente };
+    this.clienteDialog = true;
+  }
+
+  deleteCliente(cliente: any) {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete ' + cliente.name + '?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.clientes = this.clientes.filter(val => val.id !== cliente.id);
+        this.cliente = {};
+        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Cliente Deleted', life: 3000 });
+      }
+    });
+  }
+
+  hideDialog() {
+    this.clienteDialog = false;
+    this.submitted = false;
+  }
+
+  saveCliente() {
+    this.submitted = true;
+
+    if (this.cliente.name.trim()) {
+      if (this.cliente.id) {
+        this.clientes[this.findIndexById(this.cliente.id)] = this.cliente;
+        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Cliente Updated', life: 3000 });
+      }
+      else {
+        this.cliente.id = this.createId();
+        this.cliente.image = 'cliente-placeholder.svg';
+        this.clientes.push(this.cliente);
+        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Cliente Created', life: 3000 });
+      }
+
+      this.clientes = [...this.clientes];
+      this.clienteDialog = false;
+      this.cliente = {};
+    }
+  }
+
+  findIndexById(id: string): number {
+    let index = -1;
+    for (let i = 0; i < this.clientes.length; i++) {
+      if (this.clientes[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+
+    return index;
+  }
+
+  createId(): string {
+    let id = '';
+    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < 5; i++) {
+      id += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return id;
+  }
 }
